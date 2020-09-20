23157e945fe885e774ff3da576479089e3fcf6d7
= <shakirck333@gmail.com>
2020-09-20 23:11:20 +0530

    fix ui issues

--- components/ModalLoading.js
+++ components/ModalLoading.js
@@ -3,38 +3,28 @@ import { Grid, makeStyles } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "300px",
-    height: "600px",
+    width: "100%",
+    height: "500px",
     // marginBottom: ,
     backgroundColor: "#222a39",
+    position: "relative",
     padding: 10,
   },
 }));
 export default function ModalLoading() {
   const classes = useStyles();
   return (
-    <Grid
-      container
-      justify="center"
-      alignContent="center"
-      spacing={2}
-      direction="row"
-    >
-      <Grid container className={classes.root}>
-        <Grid container item xs={12} md={4}>
-          <Skeleton />
-        </Grid>
-        <Grid container item xs={12} md={6}>
-          <Grid
-            item
-            md={12}
-            justify="center"
-            sm={6}
-            className={classes.textStyle}
-          >
-            <Skeleton />
-          </Grid>
-        </Grid>
+    <Grid className={classes.root} container xs={12}>
+      <Grid item xs={12} md={6}>
+        <Skeleton variant="rect" animation="wave" height={400} />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Skeleton ariant="rect" animation="wave" />
+        <Skeleton ariant="rect" animation="wave" />
+        <Skeleton ariant="rect" animation="wave" />
+        <Skeleton ariant="rect" animation="wave" />
+        <Skeleton ariant="rect" animation="wave" />
+        <Skeleton ariant="rect" animation="wave" />
       </Grid>
     </Grid>
   );
